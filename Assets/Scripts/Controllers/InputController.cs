6af8b2a830e753c1a886830d69cbf674264422ff
amg13g <amg13g@my.fsu.edu>
2018-05-09 07:12:22 -0400

    File Structure Changes

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseController : MonoBehaviour {
+public class InputController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
